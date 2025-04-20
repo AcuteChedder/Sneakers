d58864629c59a52980b9a1dca7baa8b26ff5aa37
AcuteChedder <nubik8653@gmail.com>
2025-04-20 22:19:03 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+  import Header from "./components/Header.vue"
 </script>
 
 <template>
-  
+  <div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+    <Header/>
+
+  </div>
 </template>
 
 <style scoped> 
